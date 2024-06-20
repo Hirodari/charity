f1c7c8479a90f333a5a143827cb3e2f372531599
Fred B <fredbitenyo@gmail.com>
2024-06-20 23:48:22 +0200

    unistalling the application

--- push-image.sh
+++ push-image.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 echo "build an image"
-docker build -t 181224260758.dkr.ecr.us-east-1.amazonaws.com/charity:1.0 .
+docker build -t hirodaridevdock/charity-website-image:2.0 .
 
 echo "login to docker hub"
-aws ecr get-login-password | docker login --username AWS --password-stdin \
-	181224260758.dkr.ecr.us-east-1.amazonaws.com
+echo "$DOCKER_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 echo "push the image in the docker hub"
-docker push 181224260758.dkr.ecr.us-east-1.amazonaws.com/charity:1.0
+docker push hirodaridevdock/charity-website-image:2.0
